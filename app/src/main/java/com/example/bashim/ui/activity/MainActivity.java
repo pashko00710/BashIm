fe4768bf45ddd4a42b848669b18ea1c0740a2b27
Pavel Arkhipov <pashko00710@yandex.ru>
2016-06-01 10:09:45 +0300

    I moved on packages

---
+++ app/src/main/java/com/example/bashim/ui/activity/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.bashim.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.bashim.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+}
